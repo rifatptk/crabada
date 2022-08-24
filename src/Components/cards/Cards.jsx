c6348a57e1f5d8eec6218a4e2468daf5bdbde0b9
rifatptk <rifatptk@gmail.com>
2022-08-24 16:12:11 +0600

    single card completed

--- src/Components/cards/Cards.jsx
+++ src/Components/cards/Cards.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./cards.scss";
 import cardbg from "../../assets/images/cardbg.webp";
 import cardpaper from "../../assets/images/cardpaper.webp";
+import cardimagebg from "../../assets/images/cardimagebg.webp";
+import cardimage1 from "../../assets/images/cardimage1.webp";
 
 const Cards = () => {
   return (
@@ -10,6 +12,17 @@ const Cards = () => {
         <div className="infoCard ">
           <img src={cardbg} className="cardbg" alt="" />
           <img src={cardpaper} className="cardpaper" alt="" />
+          <div className="cardContent">
+            <div>
+              <img src={cardimagebg} alt="" />
+              <img src={cardimage1} alt="" />
+            </div>
+            <h2>Earn Rewards Effortlessly</h2>
+            <p>
+              Earn rewards in idle gameplay as you send your Crabada to Battle
+              anytime, anywhere!
+            </p>
+          </div>
         </div>
         <div className="infoCard ">
           <img src={cardbg} className="cardbg" alt="" />
