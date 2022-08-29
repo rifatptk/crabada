e582522425372a1553c885562eb092ac3c24f10a
rifatptk <rifatptk@gmail.com>
2022-08-29 12:46:15 +0600

    cards animation added

--- src/Components/cards/Cards.jsx
+++ src/Components/cards/Cards.jsx
@@ -4,57 +4,68 @@ import cardbg from "../../assets/images/cardbg.webp";
 import cardpaper from "../../assets/images/cardpaper.webp";
 import cardimagebg from "../../assets/images/cardimagebg.webp";
 import cardimage1 from "../../assets/images/cardimage1.webp";
+import { motion } from "framer-motion";
 
+const cardsData = [
+  {
+    id: 1,
+    cardImageBg: cardimagebg,
+    cardImage: cardimage1,
+    title: "Earn Rewards Effortlessly",
+    description:
+      "Earn rewards in idle gameplay as you send your Crabada to Battle anytime, anywhere!",
+  },
+  {
+    id: 2,
+    cardImageBg: cardimagebg,
+    cardImage: cardimage1,
+    title: "Go on an Exciting Adventure",
+    description:
+      "Discover rich new lands and engage in challenging boss fights as you progress through Adventure Mode!",
+  },
+  {
+    id: 3,
+    cardImageBg: cardimagebg,
+    cardImage: cardimage1,
+    title: "Expand your Crabada Forces",
+    description:
+      "Breed powerful and unique Crabada, each with their ow strengths and abilities to be used in battle!",
+  },
+];
 const Cards = () => {
+  const cardsAnimate = {
+    hidden: { opacity: 0 },
+    shown: { opacity: 1 },
+  };
+
   return (
     <div id="cardsContainer">
-      <div id="cards" className="">
-        <div className="infoCard ">
-          <img src={cardbg} className="cardbg" alt="" />
-          <img src={cardpaper} className="cardpaper" alt="" />
-          <div className="cardContent">
-            <div>
-              <img src={cardimagebg} alt="" />
-              <img src={cardimage1} alt="" />
+      <motion.div
+        id="cards"
+        initial={"hidden"}
+        whileInView={"shown"}
+        viewport={{ once: false, amount: 0.5 }}
+        transition={{ staggerChildren: 0.2 }}
+      >
+        {cardsData.map((cardData) => (
+          <motion.div
+            variants={cardsAnimate}
+            className="infoCard "
+            key={cardData.id}
+          >
+            <img src={cardbg} className="cardbg" alt="" />
+            <img src={cardpaper} className="cardpaper" alt="" />
+            <div className="cardContent">
+              <div>
+                <img src={cardData.cardImageBg} alt="" />
+                <img src={cardData.cardImage} alt="" />
+              </div>
+              <h2>{cardData.title}</h2>
+              <p>{cardData.description}</p>
             </div>
-            <h2>Earn Rewards Effortlessly</h2>
-            <p>
-              Earn rewards in idle gameplay as you send your Crabada to Battle
-              anytime, anywhere!
-            </p>
-          </div>
-        </div>
-        <div className="infoCard ">
-          <img src={cardbg} className="cardbg" alt="" />
-          <img src={cardpaper} className="cardpaper" alt="" />
-          <div className="cardContent">
-            <div>
-              <img src={cardimagebg} alt="" />
-              <img src={cardimage1} alt="" />
-            </div>
-            <h2>Go on an Exciting Adventure</h2>
-            <p>
-              Discover rich new lands and engage in challenging boss fights as
-              you progress through Adventure Mode!
-            </p>
-          </div>
-        </div>
-        <div className="infoCard ">
-          <img src={cardbg} className="cardbg" alt="" />
-          <img src={cardpaper} className="cardpaper" alt="" />
-          <div className="cardContent">
-            <div>
-              <img src={cardimagebg} alt="" />
-              <img src={cardimage1} alt="" />
-            </div>
-            <h2>Expand your Crabada Forces</h2>
-            <p>
-              Breed powerful and unique Crabada, each with their ow strengths
-              and abilities to be used in battle!
-            </p>
-          </div>
-        </div>
-      </div>
+          </motion.div>
+        ))}
+      </motion.div>
     </div>
   );
 };
