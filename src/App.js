cee58872de5794e460d9402ef184193eee9cc79d
rifatptk <rifatptk@gmail.com>
2022-08-31 10:02:20 +0600

    roadmap added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import "./App.css";
 import Book from "./Components/book/Book";
 import Cards from "./Components/cards/Cards";
 import Discover from "./Components/discover/Discover";
-import Download from "./Components/download/Download";
+// import Download from "./Components/download/Download";
 import Header from "./Components/Header/Header";
 import Hero from "./Components/Hero/Hero";
+import Roadmap from "./Components/Roadmap/Roadmap";
 
 function App() {
   return (
@@ -14,7 +15,8 @@ function App() {
       <Discover />
       <Cards />
       <Book />
-      <Download />
+      {/* <Download /> */}
+      <Roadmap />
     </div>
   );
 }
---
+++ src/Components/Roadmap/Roadmap.jsx
@@ -0,0 +1,11 @@
+import "./Roadmap.scss";
+
+const Roadmap = () => {
+  return (
+    <div id="roadmap">
+      <div id="roadmapContainer">afgasdgad</div>
+    </div>
+  );
+};
+
+export default Roadmap;
