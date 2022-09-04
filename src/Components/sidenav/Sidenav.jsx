971338de49ecb40290a5079de3c8f0163e26ba38
rifatptk <rifatptk@gmail.com>
2022-09-04 15:36:06 +0600

    animation added

--- src/Components/sidenav/Sidenav.jsx
+++ src/Components/sidenav/Sidenav.jsx
@@ -3,27 +3,39 @@ import home from "../../assets/images/icon_home.webp";
 import battle from "../../assets/images/icon_battle.webp";
 import download from "../../assets/images/icon_download.webp";
 import roadmap from "../../assets/images/icon_roadmap.webp";
+import { motion } from "framer-motion";
 
 const Sidenav = () => {
   return (
-    <div id="sidenav">
-      <div className="navItem d-flex align-items-center gap-3 justify-content-end">
-        <div className="navTitle">Home</div>
-        <img src={home} alt="" className="navLogo" />
-      </div>
-      <div className="navItem d-flex align-items-center gap-3 justify-content-end">
-        <div className="navTitle">Battle</div>
-        <img src={battle} alt="" className="navLogo" />
-      </div>
+    <motion.div
+      initial={{ opacity: 0, x: 200 }}
+      animate={{ opacity: 1, x: 0 }}
+      id="sidenav"
+    >
+      <a href="#header" className="text-decoration-none">
+        <div className="navItem d-flex align-items-center gap-3 justify-content-end">
+          <div className="navTitle">Home</div>
+          <img src={home} alt="" className="navLogo" />
+        </div>
+      </a>
+      <a href="#cards" className="text-decoration-none">
+        <div className="navItem d-flex align-items-center gap-3 justify-content-end">
+          <div className="navTitle">Battle</div>
+          <img src={battle} alt="" className="navLogo" />
+        </div>
+      </a>
+
       <div className="navItem d-flex align-items-center gap-3 justify-content-end">
         <div className="navTitle">Download</div>
         <img src={download} alt="" className="navLogo" />
       </div>
-      <div className="navItem d-flex align-items-center gap-3 justify-content-end">
-        <div className="navTitle">Roadmap</div>
-        <img src={roadmap} alt="" className="navLogo" />
-      </div>
-    </div>
+      <a href="#roadmap" className="text-decoration-none">
+        <div className="navItem d-flex align-items-center gap-3 justify-content-end">
+          <div className="navTitle">Roadmap</div>
+          <img src={roadmap} alt="" className="navLogo" />
+        </div>
+      </a>
+    </motion.div>
   );
 };
 
