ce715a9986e45877337a406821e9e5983128a59a
rifatptk <rifatptk@gmail.com>
2022-09-30 11:21:13 +0600

    Crabada done as per requirements

--- src/Components/ocean/Ocean.jsx
+++ src/Components/ocean/Ocean.jsx
@@ -1,24 +1,21 @@
 import "./Ocean.scss";
-import oceanback from "../../assets/images/oceanback.webp";
-import oceanfront from "../../assets/images/oceanfront.webp";
-import lightwala from "../../assets/images/lightwala.webp";
-import cloudy from "../../assets/images/cloudy.webp";
-import cartleft from "../../assets/images/cartleft.webp";
-import cartright from "../../assets/images/cartright.webp";
-import jhulonto from "../../assets/images/jhulonto.webp";
-import camocwala from "../../assets/images/camocwala.webp";
+import bottombg from "../../assets/images/bottombg.jpg";
+import bottomPlatform from "../../assets/images/bottomPlatform.png";
+import bottomOwl from "../../assets/images/bottomOwl.png";
+import bottomRocket from "../../assets/images/bottomRocket.png";
+import bottomJewel from "../../assets/images/bottomJewel.png";
+import bottomCoins from "../../assets/images/bottomCoins.png";
 
 const Ocean = () => {
   return (
     <div id="ocean">
-      <img id="ocnbk" src={oceanback} alt="" />
-      <img id="ocnfr" src={oceanfront} alt="" />
-      <img id="lgtwala" src={lightwala} alt="" />
-      <img id="cloudy" src={cloudy} alt="" />
-      <img id="cartleft" src={cartleft} alt="" />
-      <img id="cartright" src={cartright} alt="" />
-      <img id="jhulonto" src={jhulonto} alt="" />
-      <img id="camocwala" src={camocwala} alt="" />
+      {/* <img id="ocnbk" src={oceanback} alt="" /> */}
+      <img id="bottombg" src={bottombg} alt="" />
+      <img id="bottomPlatform" src={bottomPlatform} alt="" />
+      <img id="bottomOwl" src={bottomOwl} alt="" />
+      <img id="bottomRocket" src={bottomRocket} alt="" />
+      <img id="bottomCoins" src={bottomCoins} alt="" />
+      <img id="bottomJewel" src={bottomJewel} alt="" />
     </div>
   );
 };
